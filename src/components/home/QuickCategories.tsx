ff89e0b3eadf08e0ea457600c7793960090d349d
andrewgithinji6-jpg <andrewgithinji6@gmail.com>
2026-06-05 06:47:39 +0300

    Fix: remove unused index variable in QuickCategories

--- src/components/home/QuickCategories.tsx
+++ src/components/home/QuickCategories.tsx
@@ -40,7 +40,7 @@ export const QuickCategories: React.FC = () => {
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        {categories.map((category, index) => (
+        {categories.map((category) => (
           <motion.div
             key={category.id}
             variants={itemVariants}
